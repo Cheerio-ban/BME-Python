475b1c94efb26739c0dfbfa42b93ebc8be2eda9a
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-17 22:01:29 +0100

    Liftoff!

---
+++ week-2/liftoff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Author: Precious Jacob
+Description: This module contains solution to the 
+assigment liftoff.py
+"""
+
+def main():
+    """
+    Uses only the range function to implement a countdown!
+    """
+    for _ in range(10, 0, -1):
+        print(_)
+    print("Liftoff!")
+
+
+if __name__ == '__main__':
+    main()
